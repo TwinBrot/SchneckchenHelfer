33cd35a80fa8c089011a42a4228fb82ab70d1587
Sebi <Sebi@DESKTOP-8ES7TIF.fritz.box>
2021-08-28 03:07:29 +0200

    Empty Links.ini fix

--- SchneckchenHelfer/src/de/Strobl/Main/FileManagement.java
+++ SchneckchenHelfer/src/de/Strobl/Main/FileManagement.java
@@ -19,30 +19,54 @@ public class FileManagement {
 				newFile.createNewFile();
 			}
 			Wini ini = new Wini(new File(Main.Pfad + "settings.ini"));
-			if (ini.get("Settings", "Settings.Aktivity")!=null) {
+			if (ini.get("Settings", "Settings.Aktivity") != null) {
 				ini.put("Settings", "Settings.AktivitätText", ini.get("Settings", "Settings.Aktivity"));
 				ini.remove("Settings", "Settings.Aktivity");
-				}
-			if (ini.get("Settings", "Settings.Activity2")!=null) {
+			}
+			if (ini.get("Settings", "Settings.Activity2") != null) {
 				ini.put("Settings", "Settings.AktivitätTyp", ini.get("Settings", "Settings.Activity2"));
 				ini.remove("Settings", "Settings.Activity2");
-				}
+			}
 			ini.store();
-			if (ini.get("Setup", "Token")==null) {ini.put("Setup", "Token", "NzIyODU4NDA3Mzg3ODU3MDQ3.XuqAiQ.eVkd3rLUaWuI4dEkv2ulCeSJKG8");}
-			if (ini.get("Settings", "Settings.AktivitätTyp")==null) {ini.put("Settings", "Settings.AktivitätTyp", "!hilfe");}
-			if (ini.get("Settings", "Settings.AktivitätText")==null) {ini.put("Settings", "Settings.AktivitätText", "playing");}
-			if (ini.get("Settings", "Settings.StreamLink")==null) {ini.put("Settings", "Settings.StreamLink", "https://www.twitch.tv/maudado");}
-			if (ini.get("Settings", "Settings.LogChannel")==null) {ini.put("Settings", "Settings.LogChannel", "");}
-			if (ini.get("Settings", "Settings.AFKVoice")==null) {ini.put("Settings", "Settings.AFKVoice", "no");}
-			if (ini.get("Settings", "Settings.Status")==null) {ini.put("Settings", "Settings.Status", "ONLINE");}
-			if (ini.get("ModRollen", "Channelmod")==null) {ini.put("ModRollen", "Channelmod", "[0]");}
-			if (ini.get("Dateiüberwachung", "Active")==null) {ini.put("Dateiüberwachung", "Active", "false");}
-			if (ini.get("Dateiüberwachung", "Allowed")==null) {ini.put("Dateiüberwachung", "Allowed", "[0, jpg, png, jpeg, gif, bmp, mp3, mov, mp4, m4a, webp, webm, tif, avi, psd, jpg_large, wav, txt, pdf, jfif, heic, jpglarge, 3gp]");}
-			if (ini.get("Namensüberwachung", "Active")==null) {ini.put("Namensüberwachung", "Active", "false");}
-			if (ini.get("Namensüberwachung", "Verboten")==null) {ini.put("Namensüberwachung", "Verboten", "[*~+]");}
+			if (ini.get("Setup", "Token") == null) {
+				ini.put("Setup", "Token", "NzIyODU4NDA3Mzg3ODU3MDQ3.XuqAiQ.eVkd3rLUaWuI4dEkv2ulCeSJKG8");
+			}
+			if (ini.get("Settings", "Settings.AktivitätTyp") == null) {
+				ini.put("Settings", "Settings.AktivitätTyp", "!hilfe");
+			}
+			if (ini.get("Settings", "Settings.AktivitätText") == null) {
+				ini.put("Settings", "Settings.AktivitätText", "playing");
+			}
+			if (ini.get("Settings", "Settings.StreamLink") == null) {
+				ini.put("Settings", "Settings.StreamLink", "https://www.twitch.tv/maudado");
+			}
+			if (ini.get("Settings", "Settings.LogChannel") == null) {
+				ini.put("Settings", "Settings.LogChannel", "");
+			}
+			if (ini.get("Settings", "Settings.AFKVoice") == null) {
+				ini.put("Settings", "Settings.AFKVoice", "no");
+			}
+			if (ini.get("Settings", "Settings.Status") == null) {
+				ini.put("Settings", "Settings.Status", "ONLINE");
+			}
+			if (ini.get("ModRollen", "Channelmod") == null) {
+				ini.put("ModRollen", "Channelmod", "[0]");
+			}
+			if (ini.get("Dateiüberwachung", "Active") == null) {
+				ini.put("Dateiüberwachung", "Active", "false");
+			}
+			if (ini.get("Dateiüberwachung", "Allowed") == null) {
+				ini.put("Dateiüberwachung", "Allowed",
+						"[0, jpg, png, jpeg, gif, bmp, mp3, mov, mp4, m4a, webp, webm, tif, avi, psd, jpg_large, wav, txt, pdf, jfif, heic, jpglarge, 3gp]");
+			}
+			if (ini.get("Namensüberwachung", "Active") == null) {
+				ini.put("Namensüberwachung", "Active", "false");
+			}
+			if (ini.get("Namensüberwachung", "Verboten") == null) {
+				ini.put("Namensüberwachung", "Verboten", "[*~+]");
+			}
 			ini.store();
-			
-			
+
 // Read Emotes.ini File
 			try {
 				Wini emotes = new Wini(new File(Main.Pfad + "Emotes.ini"));
@@ -53,10 +77,11 @@ public class FileManagement {
 				newFile.createNewFile();
 			}
 			Wini emotes = new Wini(new File(Main.Pfad + "Emotes.ini"));
-			if (emotes.get("Embed", "Count")==null) {emotes.put("Embed", "Count", "24");}
+			if (emotes.get("Embed", "Count") == null) {
+				emotes.put("Embed", "Count", "24");
+			}
 			emotes.store();
-			
-			
+
 // Read Strafen.ini File
 			try {
 				Wini Strafen = new Wini(new File(Main.Pfad + "Strafen.ini"));
@@ -79,12 +104,11 @@ public class FileManagement {
 				newFile.createNewFile();
 			}
 			Wini Auswertung = new Wini(new File(Main.Pfad + "ID.ini"));
-			if (Auswertung.get("Hinweise", "Counter")==null) {Auswertung.put("Hinweise", "Counter", "0");}
+			if (Auswertung.get("Hinweise", "Counter") == null) {
+				Auswertung.put("Hinweise", "Counter", "0");
+			}
 			Auswertung.store();
-			
-			
-			
-			
+
 // Read Link.ini File
 			try {
 				Wini Link = new Wini(new File(Main.Pfad + "Link.ini"));
@@ -95,15 +119,17 @@ public class FileManagement {
 				newFile.createNewFile();
 			}
 			Wini Link = new Wini(new File(Main.Pfad + "Link.ini"));
-			if (Link.get("Links").size() == 0) {
+			if (Link.isEmpty()) {
+				Link.clear();
 				Link.putComment("Links", "Add Links like this:   COUNTER = LINK");
 				Link.put("Links", "1", "discorcl.link/");
-				}
+			} else if (Link.get("Links").size() == 0) {
+				Link.clear();
+				Link.putComment("Links", "Add Links like this:   COUNTER = LINK");
+				Link.put("Links", "1", "discorcl.link/");
+			}
 			Link.store();
-			
-			
-			
-			
+
 // Users Ordner Anlegen
 			File file = new File(Main.Userpfad);
 			if (!file.exists()) {
@@ -115,9 +141,6 @@ public class FileManagement {
 				}
 			}
 
-			
-			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Fehler beim anlegen der Dateien.");
