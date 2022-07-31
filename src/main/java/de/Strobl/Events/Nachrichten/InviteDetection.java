bd7b4979c80562106ccd6f697f05648410dfd311
Twin <Twin@Computer.fritz.box>
2022-07-31 21:38:50 +0200

    Banbutton on Invite Detection

--- src/main/java/de/Strobl/Events/Nachrichten/InviteDetection.java
+++ src/main/java/de/Strobl/Events/Nachrichten/InviteDetection.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 public class InviteDetection extends ListenerAdapter {
 	private static final Logger logger = LogManager.getLogger(InviteDetection.class);
@@ -40,21 +41,21 @@ public class InviteDetection extends ListenerAdapter {
 										logger.info("Servereinladung erkannt. Author ist Mod, daher nicht gelöscht.");
 										return;
 									}
-									
 
 									event.getMember().getUser().openPrivateChannel().queue(pc -> {
-										EmbedBuilder builder = Discord.standardEmbed(Color.red, "TimeOut", event.getMember().getId(), event.getMember().getUser().getEffectiveAvatarUrl());
+										EmbedBuilder builder = Discord.standardEmbed(Color.red, "TimeOut", event.getMember().getId(),
+												event.getMember().getUser().getEffectiveAvatarUrl());
 										builder.setAuthor(event.getGuild().getName(), null, event.getGuild().getIconUrl());
 										builder.setDescription("Dein Account wurde gerade automatisch für 10 Minuten getimeoutet!");
 										builder.addField("Grund:", "Senden eines Discord-Invitelinks!", true);
-										builder.addField("Die Moderatoren werden das kontrollieren.", "Wenn sich der Verdacht nicht bestätigt, werden die Moderatoren dich wieder freischalten.", false);
+										builder.addField("Die Moderatoren werden das kontrollieren.",
+												"Wenn sich der Verdacht nicht bestätigt, werden die Moderatoren dich wieder freischalten.", false);
 										pc.sendMessageEmbeds(builder.build()).queue(msg -> {
 										}, e -> {
 										});
 										event.getMember().timeoutFor(10, TimeUnit.MINUTES).queueAfter(1, TimeUnit.SECONDS);
 									});
-									
-									
+
 									event.getMessage().delete().queue(success -> {
 										try {
 											String title = "Invite eines anderen Servers erkannt. Nachricht gelöscht!";
@@ -62,7 +63,8 @@ public class InviteDetection extends ListenerAdapter {
 											EmbedBuilder builder = Discord.standardEmbed(Color.BLUE, title, member.getId(), member.getEffectiveAvatarUrl());
 											builder.addField("Channel: " + event.getGuildChannel().getName() + "     Nachrichten Text: ", content, true);
 											TextChannel channel = event.getGuild().getTextChannelById(Settings.LogChannel);
-											channel.sendMessage("User: " + member.getAsMention() + " Notification: <@227131380058947584> <@140206875596685312>").setEmbeds(builder.build()).queue();
+											channel.sendMessage("User: " + member.getAsMention() + " Notification: <@227131380058947584> <@140206875596685312>")
+													.setEmbeds(builder.build()).setActionRow(Button.danger("ban " + member.getId(), "Ban User")).queue();
 										} catch (Exception e) {
 											logger.error("Fehler Invite Detection", e);
 										}
